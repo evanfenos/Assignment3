850cf1a359eea363c38c485664d0041a3cac2996
evanfenos <61528613+evanfenos@users.noreply.github.com>
2020-02-27 23:54:12 -0500

    Add files via upload
    Improved results but tests still failing

--- TrigFunc.java
+++ TrigFunc.java
@@ -1,8 +1,10 @@
-public class TrigFunc {	
+public class TrigFunc {
 	public double sin (double x) {
 		double result = x;
 		double term = 1;
-		for (int i = 1; i<1000000; i++) {
+		for (int i = 1; i<9000000; i++) {
+			// Defining terms separately like this removes the need for factorial
+			// Terms are based on Taylor's series given at: https://www.efunda.com/math/taylor_series/trig.cfm
 			term *= ((-1)*(x*x))/((i + 1)*(i + 2));
 			result += term;
 		}
@@ -13,7 +15,9 @@ public class TrigFunc {
 	public double cos (double x) {
 		double result = 1;
 		double term = 1;
-		for (int i = 1; i<1000000; i++) {
+		for (int i = 0; i<9000000; i++) {
+			// Defining terms separately like this removes the need for factorial
+			// Terms are based on Taylor's series given at: https://www.efunda.com/math/taylor_series/trig.cfm
 			term *= ((-1)*(x*x))/((i + 1)*(i + 2));
 			result += term;
 		}
@@ -23,9 +27,10 @@ public class TrigFunc {
 	
 	public double tan (double x) {
 		double p = Math.PI;
-		if (x != 0 && x % (p/2) == 0) {
+		if (x != 0 && (x/(p/2)) % 2 == 1) {
 			return 999999999;				// This is the best way I could think to represent undefined.
 		} else {
+			// Trig identity sin(x)/cos(x) = tan(x)
 			return (sin(x)/cos(x));
 		}
 	}
