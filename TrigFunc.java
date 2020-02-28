37b829ed1c3b0cfef1a013b4f0a0e30bed701f8d
evanfenos <61528613+evanfenos@users.noreply.github.com>
2020-02-27 23:07:37 -0500

    Add files via upload

--- TrigFunc.java
+++ TrigFunc.java
@@ -1,22 +1,41 @@
-
 public class TrigFunc {	
 	public double sin (double x) {
-
+		double result = x;
+		double term = 1;
+		int termNum = 1;
+		for (int i = 1; i<10000; i++) {
+			term *= ((-1)*(x*x))/((termNum + 1)*(termNum + 2));
+			result += term;
+		}
+		
+		return result;
 	}
 	
 	public double cos (double x) {
-
+		double result = 1;
+		double term = 1;
+		int termNum = 0;
+		for (int i = 1; i<10000; i++) {
+			term *= ((-1)*(x*x))/((termNum + 1)*(termNum + 2));
+			result += term;
+		}
+		
+		return result;
 	}
 	
 	public double tan (double x) {
-
+		return (sin(x)/cos(x));
 	}
 	
 	public double degToRad (double x) {
-		
+		double p = Math.PI;
+		double xRad = x*(p/180);
+		return xRad;
 	}
 	
 	public double radToDeg (double x) {
-		
+		double p = Math.PI;
+		double xDeg = x*(180/p);
+		return xDeg;
 	}
 }
