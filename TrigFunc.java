c8e6affba39c5657874b7a8d0aef0cc07724385f
evanfenos <61528613+evanfenos@users.noreply.github.com>
2020-02-28 23:36:44 -0500

    Add files via upload
    Added/fixed some tests for clarity's sake. Also fixed all bugs

--- TrigFunc.java
+++ TrigFunc.java
@@ -2,11 +2,17 @@ public class TrigFunc {
 	public double sin (double x) {
 		double result = x;
 		double term = 1;
-		for (int i = 1; i<9000000; i++) {
+		int termNum = 1;
+		for (int i = 1; i<1000000; i++) {
 			// Defining terms separately like this removes the need for factorial
 			// Terms are based on Taylor's series given at: https://www.efunda.com/math/taylor_series/trig.cfm
-			term *= ((-1)*(x*x))/((i + 1)*(i + 2));
+			if (i == 1) {
+				term *= ((-1)*(x*x*x))/((termNum + 1)*(termNum + 2));		// Pattern starts unevenly
+			} else {
+				term *= ((-1)*(x*x))/((termNum + 1)*(termNum + 2));
+			}
 			result += term;
+			termNum += 2;
 		}
 		
 		return result;
@@ -15,11 +21,13 @@ public class TrigFunc {
 	public double cos (double x) {
 		double result = 1;
 		double term = 1;
-		for (int i = 0; i<9000000; i++) {
+		int termNum = 0;
+		for (int i = 0; i<1000000; i++) {
 			// Defining terms separately like this removes the need for factorial
 			// Terms are based on Taylor's series given at: https://www.efunda.com/math/taylor_series/trig.cfm
-			term *= ((-1)*(x*x))/((i + 1)*(i + 2));
+			term *= ((-1)*(x*x))/((termNum + 1)*(termNum + 2));
 			result += term;
+			termNum += 2;
 		}
 		
 		return result;
@@ -27,7 +35,7 @@ public class TrigFunc {
 	
 	public double tan (double x) {
 		double p = Math.PI;
-		if (x != 0 && (x/(p/2)) % 2 == 1) {
+		if (x != 0 && (((x/(p/2)) % 2 == 1) || (x/(p/2)) % 2 == -1)) {
 			return 999999999;				// This is the best way I could think to represent undefined.
 		} else {
 			// Trig identity sin(x)/cos(x) = tan(x)
--- TrigFuncTest.java
+++ TrigFuncTest.java
@@ -15,24 +15,27 @@ public class TrigFuncTest {
 		double p = Math.PI;
 		
 		/* Axis values set (All inputs lie on the axes */
-		assertEquals(trig.sin(0), 0, 0.0001);
-		assertEquals(trig.sin(p/2), 1, 0.0001);
-		assertEquals(trig.sin(p), 0, 0.0001);
-		assertEquals(trig.sin((3/4)*p), -1, 0.0001);
+		assertEquals(trig.sin(0), Math.sin(0), 0.001);
+		assertEquals(trig.sin(p/2), Math.sin(p/2), 0.001);
+		assertEquals(trig.sin(p), Math.sin(p), 0.001);
+		assertEquals(trig.sin((3/2)*p), Math.sin((3/2)*p), 0.001);
 		
 		/* Extra whole number results (All inputs are found between axes) */
-		assertEquals(trig.sin(p/3), Math.sqrt(3)/2, 0.0001);
-		assertEquals(trig.sin(p/6), 1/2, 0.0001);
+		assertEquals(trig.sin(p/3), Math.sin(p/3), 0.001);
+		assertEquals(trig.sin(p/6), Math.sin(p/6), 0.001);
 		
 		/* Greater than 2*pi set */
-		assertEquals(trig.cos((5/2)*p), 1, 0.0001);
-		assertEquals(trig.cos(3*p), 0, 0.0001);
-		assertEquals(trig.cos((7/2)*p), -1, 0.0001);
-		assertEquals(trig.cos(4*p), 0, 0.0001);
+		assertEquals(trig.sin((5/2)*p), Math.sin((5/2)*p), 0.001);
+		assertEquals(trig.sin(3*p), Math.sin(3*p), 0.001);
+		assertEquals(trig.sin((7/2)*p), Math.sin((7/2)*p), 0.001);
+		assertEquals(trig.sin(4*p), Math.sin(4*p), 0.001);
 		
 		/* Negative set */
-		assertEquals(trig.sin(p/-2), -1, 0.0001);
-		assertEquals(trig.sin(p/-6), -1/2, 0.0001);
+		assertEquals(trig.sin(p/-2), Math.sin(p/-2), 0.001);
+		assertEquals(trig.sin(p/-6), Math.sin(p/-6), 0.001);
+		
+		/* Small Number */
+		assertEquals(trig.sin(0.0001), Math.sin(0.0001), 0.001);
 	}
 
 	@Test
@@ -41,24 +44,27 @@ public class TrigFuncTest {
 		double p = Math.PI;
 		
 		/* Axis values set (All inputs lie on the axes */
-		assertEquals(trig.cos(0), 1, 0.0001);
-		assertEquals(trig.cos(p/2), 0, 0.0001);
-		assertEquals(trig.cos(p), -1, 0.0001);
-		assertEquals(trig.cos((3/4)*p), 0, 0.0001);
+		assertEquals(trig.cos(0), Math.cos(0), 0.001);
+		assertEquals(trig.cos(p/2), Math.cos(p/2), 0.001);
+		assertEquals(trig.cos(p), Math.cos(p), 0.001);
+		assertEquals(trig.cos((3/2)*p), Math.cos((3/2)*p), 0.001);
 		
 		/* Extra whole number results (All inputs are found between axes) */
-		assertEquals(trig.cos(p/3), 1/2, 0.0001);
-		assertEquals(trig.cos(p/6), Math.sqrt(3)/2, 0.0001);
+		assertEquals(trig.cos(p/3), Math.cos(p/3), 0.001);
+		assertEquals(trig.cos(p/6), Math.cos(p/6), 0.001);
 		
 		/* Greater than 2*pi set */
-		assertEquals(trig.cos((5/2)*p), 0, 0.0001);
-		assertEquals(trig.cos(3*p), -1, 0.0001);
-		assertEquals(trig.cos((7/2)*p), 0, 0.0001);
-		assertEquals(trig.cos(4*p), 1, 0.0001);
+		assertEquals(trig.cos((5/2)*p), Math.cos((5/2)*p), 0.001);
+		assertEquals(trig.cos(3*p), Math.cos(3*p), 0.001);
+		assertEquals(trig.cos((7/2)*p), Math.cos((7/2)*p), 0.001);
+		assertEquals(trig.cos(4*p), Math.cos(4*p), 0.001);
 		
 		/* Negative set */
-		assertEquals(trig.cos(-1*p), -1, 0.0001);
-		assertEquals(trig.cos(p/-3), 1/2, 0.0001);
+		assertEquals(trig.cos(-1*p), Math.cos(-1*p), 0.001);
+		assertEquals(trig.cos(p/-3), Math.cos(p/-3), 0.001);
+		
+		/* Small Number */
+		assertEquals(trig.cos(0.0001), Math.cos(0.0001), 0.001);
 	}
 
 	@Test
@@ -67,22 +73,24 @@ public class TrigFuncTest {
 		double p = Math.PI;
 		
 		/* Axis values set (All inputs lie on the axes */
-		assertEquals(trig.tan(0), 0, 0.0001);
-		assertEquals(trig.tan(p/2), 999999999, 0.0001);		// This is meant to identify an undefined result.
-		assertEquals(trig.tan(p), 0, 0.0001);
-		assertEquals(trig.tan((3/4)*p), 999999999, 0.0001);			// This is meant to identify an undefined result.
+		assertEquals(trig.tan(0), Math.tan(0), 0.001);
+		assertEquals(trig.tan(p/2), 999999999, 0.001);		// This is meant to identify an undefined result.
+		assertEquals(trig.tan(p), Math.tan(p), 0.001);
 		
 		/* Extra whole number results (All inputs are found between axes) */
-		assertEquals(trig.tan(p/3), Math.sqrt(3), 0.0001);
-		assertEquals(trig.tan(p/6), 1/Math.sqrt(3), 0.0001);
+		assertEquals(trig.tan(p/3), Math.tan(p/3), 0.001);
+		assertEquals(trig.tan(p/6), Math.tan(p/6), 0.001);
 		
 		/* Greater than 2*pi set */
-		assertEquals(trig.tan(3*p), 0, 0.0001);
-		assertEquals(trig.tan(4*p), 0, 0.0001);
+		assertEquals(trig.tan(3*p), Math.tan(3*p), 0.001);
+		assertEquals(trig.tan(4*p), Math.tan(4*p), 0.001);
 		
 		/* Negative set */
-		assertEquals(trig.tan(p/-2), 999999999, 0.0001);		// This is meant to identify an undefined result.
-		assertEquals(trig.tan(p/-6), 1/Math.sqrt(3), 0.0001);
+		assertEquals(trig.tan(p/-2), 999999999, 0.001);		// This is meant to identify an undefined result.
+		assertEquals(trig.tan(p/-6), Math.tan(p/-6), 0.001);
+		
+		/* Small Number */
+		assertEquals(trig.tan(0.0001), Math.tan(0.0001), 0.001);
 	}
 
 	@Test
@@ -90,11 +98,11 @@ public class TrigFuncTest {
 		TrigFunc trig = new TrigFunc();
 		double p = Math.PI;
 		
-		assertEquals(trig.degToRad(180), p, 0.0001);
-		assertEquals(trig.degToRad(60), p/3, 0.0001);
-		assertEquals(trig.degToRad(30), p/6, 0.0001);
-		assertEquals(trig.degToRad(-180), -p, 0.0001);
-		assertEquals(trig.degToRad(-60), -1*p/3, 0.0001);
+		assertEquals(trig.degToRad(180), p, 0.001);
+		assertEquals(trig.degToRad(60), p/3, 0.001);
+		assertEquals(trig.degToRad(30), p/6, 0.001);
+		assertEquals(trig.degToRad(-180), -p, 0.001);
+		assertEquals(trig.degToRad(-60), -1*p/3, 0.001);
 	}
 
 	@Test
@@ -102,10 +110,10 @@ public class TrigFuncTest {
 		TrigFunc trig = new TrigFunc();
 		double p = Math.PI;
 		
-		assertEquals(trig.radToDeg(p), 180, 0.0001);
-		assertEquals(trig.radToDeg(p/3), 60, 0.0001);
-		assertEquals(trig.radToDeg(p/6), 30, 0.0001);
-		assertEquals(trig.radToDeg(-1*p), -180, 0.0001);
-		assertEquals(trig.radToDeg(-1*p/3), -60, 0.0001);
+		assertEquals(trig.radToDeg(p), 180, 0.001);
+		assertEquals(trig.radToDeg(p/3), 60, 0.001);
+		assertEquals(trig.radToDeg(p/6), 30, 0.001);
+		assertEquals(trig.radToDeg(-1*p), -180, 0.001);
+		assertEquals(trig.radToDeg(-1*p/3), -60, 0.001);
 	}
 }
